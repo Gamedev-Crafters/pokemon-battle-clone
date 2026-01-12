f0ddf82936ba61295b4eb163a6004f19a971fba2
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-12 12:20:15 +0100

    test: elemental type test cases

---
+++ Assets/Pokemon Battle Clone/Tests/ElementalTypeTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Pokemon_Battle_Clone.Runtime.Core;
+
+namespace Pokemon_Battle_Clone.Tests
+{
+    public class ElementalTypeTests
+    {
+        [Test]
+        public void NeutralAgainstMoveType()
+        {
+            var moveType = ElementalType.Fire;
+            var pokemonType = ElementalType.Dark;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType), Is.EqualTo(1f));
+        }
+
+        [Test]
+        public void WeakAgainstMoveType()
+        {
+            var moveType = ElementalType.Grass;
+            var pokemonType = ElementalType.Water;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType), Is.EqualTo(2f));
+        }
+
+        [Test]
+        public void ResistanceAgainstMoveType()
+        {
+            var moveType = ElementalType.Grass;
+            var pokemonType = ElementalType.Fire;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType), Is.EqualTo(0.5f));
+        }
+
+        [Test]
+        public void NeutralizeMoveEffectiveness()
+        {
+            var moveType = ElementalType.Water;
+            var pokemonType1 = ElementalType.Grass;
+            var pokemonType2 = ElementalType.Fire;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType1, pokemonType2), Is.EqualTo(1f));
+        }
+
+        [Test]
+        public void IncreaseMoveEffectiveness()
+        {
+            var moveType = ElementalType.Water;
+            var pokemonType1 = ElementalType.Ground;
+            var pokemonType2 = ElementalType.Rock;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType1, pokemonType2), Is.EqualTo(4f));
+        }
+
+        [Test]
+        public void ImmuniseMoveEffectiveness()
+        {
+            var moveType = ElementalType.Psychic;
+            var pokemonType1 = ElementalType.Poison;
+            var pokemonType2 = ElementalType.Dark;
+            
+            Assert.That(moveType.EffectivenessAgainst(pokemonType1, pokemonType2), Is.EqualTo(0f));
+        }
+    }
+}
