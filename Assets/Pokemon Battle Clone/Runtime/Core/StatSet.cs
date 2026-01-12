5c5bc99fce6fd76b6ee2f6154c07cb812955fe01
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-12 16:04:50 +0100

    feat: stats data

---
+++ Assets/Pokemon Battle Clone/Runtime/Core/StatSet.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Pokemon_Battle_Clone.Runtime.Core
+{
+    public class StatSet
+    {
+        public int HP { get; private set; }
+        public int Attack { get; private set; }
+        public int SpcAttack { get; private set; }
+        public int Defense { get; private set; }
+        public int SpcDefense { get; private set; }
+        public int Speed { get; private set; }
+
+        public int Sum => HP + Attack + SpcAttack + Defense + SpcDefense + Speed;
+        
+        private int _maxStatValue;
+        private int _minStatValue;
+
+        public StatSet()
+        {
+            HP = 0;
+            Attack = 0;
+            SpcAttack = 0;
+            Defense = 0;
+            SpcDefense = 0;
+            Speed = 0;
+            
+            _maxStatValue = int.MaxValue;
+            _minStatValue = int.MinValue;
+        }
+
+        public StatSet(int hp, int attack, int spcAttack, int defense, int spcDefense, int speed)
+        {
+            HP = hp;
+            Attack = attack;
+            SpcAttack = spcAttack;
+            Defense = defense;
+            SpcDefense = spcDefense;
+            Speed = speed;
+            
+            _maxStatValue = int.MaxValue;
+            _minStatValue = int.MinValue;
+        }
+
+        public static StatSet BlankEVsSet()
+        {
+            var statSet = new StatSet();
+            statSet.SetMinMaxValues(0, 252);
+
+            return statSet;
+        }
+
+        public static StatSet BlankIVsSet()
+        {
+            var statSet = new StatSet();
+            statSet.SetMinMaxValues(0, 31);
+            
+            return statSet;
+        }
+
+        /// <summary>
+        /// Set min/max stats values and clamps them
+        /// </summary>
+        public void SetMinMaxValues(int min, int max)
+        {
+            Assert.IsTrue(min <= max);
+            
+            _maxStatValue = max;
+            _minStatValue = min;
+            
+            ClampStats();
+        }
+        
+        private void ClampStats()
+        {
+            HP = Math.Clamp(HP, _minStatValue, _maxStatValue);
+            Attack = Math.Clamp(Attack, _minStatValue, _maxStatValue);
+            SpcAttack = Math.Clamp(SpcAttack, _minStatValue, _maxStatValue);
+            Defense = Math.Clamp(Defense, _minStatValue, _maxStatValue);
+            SpcDefense = Math.Clamp(SpcDefense, _minStatValue, _maxStatValue);
+            Speed = Math.Clamp(Speed, _minStatValue, _maxStatValue);
+        }
+    }
+}
---
+++ Assets/Pokemon Battle Clone/Runtime/Core/StatsData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Pokemon_Battle_Clone.Runtime.Core
+{
+    public class StatsData
+    {
+        private int _level;
+        public int Level
+        {
+            get => _level;
+            set => _level = Math.Clamp(value, 0, 100);
+        }
+        private StatSet _evs;
+        public StatSet EVs
+        {
+            get => _evs;
+            set
+            {
+                Assert.IsTrue(value.Sum <= 508);
+                _evs = value;
+            }
+        }
+        public StatSet IVs { get; set; }
+        public StatSet BaseStats { get; }
+        public StatSet Stats { get; private set; }
+
+        public StatsData(int level, StatSet baseStats)
+        {
+            Level = level;
+            BaseStats = baseStats;
+            EVs = StatSet.BlankEVsSet();
+            IVs = StatSet.BlankIVsSet();
+        }
+
+        private static StatSet CalculateStats(int level, StatSet baseStats, StatSet evs, StatSet ivs)
+        {
+            return new StatSet(
+                CalculateHPStat(level, baseStats.HP, evs.HP, ivs.HP),
+                CalculateStat(level, baseStats.Attack, evs.Attack, ivs.Attack),
+                CalculateStat(level, baseStats.SpcAttack, evs.SpcAttack, ivs.SpcAttack),
+                CalculateStat(level, baseStats.Defense, evs.Defense, ivs.Defense),
+                CalculateStat(level, baseStats.SpcDefense, evs.SpcDefense, ivs.SpcDefense),
+                CalculateStat(level, baseStats.Speed, evs.Speed, ivs.Speed));
+        }
+
+        private static int CalculateHPStat(int level, int baseHP, int hpEV, int hpIV)
+        {
+            return Mathf.FloorToInt((2f * baseHP + hpIV + Mathf.FloorToInt(hpEV / 4f)) * level / 100) + level + 10;
+        }
+
+        private static int CalculateStat(int level, int baseStat, int ev, int iv)
+        {
+            var nature = 0f;
+            return Mathf.FloorToInt((Mathf.FloorToInt((2f * baseStat + iv + Mathf.FloorToInt(ev /4f)) * level / 100) + 5) * nature);
+        }
+    }
+}
