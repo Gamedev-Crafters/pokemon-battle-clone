6a1eab18e019e34f896d3adfebd367ba1d74be64
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-13 20:11:51 +0100

    test: calculate damage

--- Assets/Pokemon Battle Clone/Runtime/Core/PokemonUtils.cs
+++ Assets/Pokemon Battle Clone/Runtime/Core/PokemonUtils.cs
@@ -4,15 +4,14 @@ namespace Pokemon_Battle_Clone.Runtime.Core
 {
     public static class PokemonUtils
     {
-        public static int CalculateDamage(int level, int attack, int defense, int power, float targets, float weather, float critical, float random, float stab, float effectiveness, float burn, float other = 1f)
+        private static readonly Random rnd = new Random();
+        
+        public static int CalculateDamage(int level, int attack, int defense, int power, float targets, float weather, float critical, float random, float stab, float effectiveness, float burn, float other)
         {
             var damage = ( ( 2 * level / 5f + 2 ) * power * attack / defense / 50 + 2 ) * targets * weather * critical * random * stab * effectiveness * burn * other;
             return Math.Max(1, (int)damage);
         }
 
-        public static float RandomDamageFactor()
-        {
-            return 1;
-        }
+        public static float RandomDamageFactor() => rnd.Next(85, 101) / 100f;
     }
 }
---
+++ Assets/Pokemon Battle Clone/Tests/DamageTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Pokemon_Battle_Clone.Runtime.Core;
+
+namespace Pokemon_Battle_Clone.Tests
+{
+    public class DamageTests
+    {
+        [Test]
+        public void AttackDamage()
+        {
+            var damage = PokemonUtils.CalculateDamage(level: 75, attack: 123, defense: 163, power: 65, targets: 1f,
+                weather: 1f, critical: 1f, random: PokemonUtils.RandomDamageFactor(), stab: 1.5f, effectiveness: 4f,
+                burn: 1f, other: 1f);
+            
+            Assert.That(damage, Is.InRange(170, 200));
+        }
+    }
+}
