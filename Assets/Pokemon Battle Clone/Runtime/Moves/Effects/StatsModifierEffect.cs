b45c1691af935e61a432e213ade0ed426ae4b634
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-14 11:34:03 +0100

    feat: stats modifier move effect

---
+++ Assets/Pokemon Battle Clone/Runtime/Moves/Effects/StatsModifierEffect.cs
@@ -0,0 +1,24 @@
+﻿using Pokemon_Battle_Clone.Runtime.Core;
+
+namespace Pokemon_Battle_Clone.Runtime.Moves.Effects
+{
+    public class StatsModifierEffect : IMoveEffect
+    {
+        private readonly bool _applyToTarget;
+        private readonly StatSet _statsModifier;
+
+        public StatsModifierEffect(bool applyToTarget, StatSet statsModifier)
+        {
+            _applyToTarget = applyToTarget;
+            _statsModifier = statsModifier;
+        }
+        
+        public void Apply(Move move, Pokemon user, Pokemon target)
+        {
+            if (_applyToTarget)
+                target.Stats.Modifiers.Apply(_statsModifier);
+            else
+                user.Stats.Modifiers.Apply(_statsModifier);
+        }
+    }
+}
