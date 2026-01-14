49ef559698cf9acaf4457b1aa46791180feccaa5
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-14 16:12:39 +0100

    feat: pokemon team class

---
+++ Assets/Pokemon Battle Clone/Runtime/Core/Team.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Pokemon_Battle_Clone.Runtime.Core
+{
+    public class Team
+    {
+        private readonly List<Pokemon> _pokemonList = new List<Pokemon>();
+        public IReadOnlyList<Pokemon> PokemonList => _pokemonList;
+
+        public bool Add(Pokemon pokemon)
+        {
+            if (_pokemonList.Count >= 6) return false;
+            
+            _pokemonList.Add(pokemon);
+            return true;
+        }
+    }
+}
