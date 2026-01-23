f0b78c6d9b66a7b4a3358627d457fa14fa716881
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-01-23 12:29:27 +0100

    refactor: extract RandomProvider class

--- Assets/Pokemon Battle Clone/Runtime/Core/Domain/PokemonUtils.cs
+++ Assets/Pokemon Battle Clone/Runtime/Core/Domain/PokemonUtils.cs
@@ -4,14 +4,12 @@ namespace Pokemon_Battle_Clone.Runtime.Core.Domain
 {
     public static class PokemonUtils
     {
-        private static readonly Random rnd = new Random();
-        
         public static int CalculateDamage(int level, int attack, int defense, int power, float targets, float weather, float critical, float random, float stab, float effectiveness, float burn, float other)
         {
             var damage = ( ( 2 * level / 5f + 2 ) * power * attack / defense / 50 + 2 ) * targets * weather * critical * random * stab * effectiveness * burn * other;
             return Math.Max(1, (int)damage);
         }
 
-        public static float RandomDamageFactor() => rnd.Next(85, 101) / 100f;
+        public static float RandomDamageFactor() => RandomProvider.Range(85, 101) / 100f;
     }
 }
---
+++ Assets/Pokemon Battle Clone/Runtime/Core/Domain/RandomProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon_Battle_Clone.Runtime.Core.Domain
+{
+    public static class RandomProvider
+    {
+        private static readonly System.Random _random = new System.Random();
+        
+        public static int Range(int min, int max) => _random.Next(min, max);
+        public static int Next() => _random.Next();
+    }
+}
