11268b377dd3b63ed9f58ec10d05fdc44fc673ec
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2026-02-28 10:20:39 +0100

    docs: comentarios PokemonUtils

--- Assets/Pokemon Battle Clone/Runtime/Core/Domain/PokemonUtils.cs
+++ Assets/Pokemon Battle Clone/Runtime/Core/Domain/PokemonUtils.cs
@@ -3,8 +3,10 @@ using Pokemon_Battle_Clone.Runtime.RNG;
 
 namespace Pokemon_Battle_Clone.Runtime.Core.Domain
 {
+    //¿Qué es PokemonUtils
     public static class PokemonUtils
     {
+        //Métodos con tantos parámetros son dificiles de leer y entender, ¿se te ocurre algo para facilitar que se entienda mejor?
         public static int CalculateDamage(int level, int attack, int defense, int power, float targets, float weather, float critical, float random, float stab, float effectiveness, float burn, float other)
         {
             var damage = ( ( 2 * level / 5f + 2 ) * power * attack / defense / 50 + 2 ) * targets * weather * critical * random * stab * effectiveness * burn * other;
