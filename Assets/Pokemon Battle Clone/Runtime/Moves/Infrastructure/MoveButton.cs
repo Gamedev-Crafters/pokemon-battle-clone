e5b3a2a102dab650f46b3df83d9d08214c336162
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-03-09 18:37:55 +0100

    feat: update move buttons visual

--- Assets/Pokemon Battle Clone/Runtime/Moves/Infrastructure/MoveButton.cs
+++ Assets/Pokemon Battle Clone/Runtime/Moves/Infrastructure/MoveButton.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-using System.Threading.Tasks;
-using Pokemon_Battle_Clone.Runtime.Moves.Domain;
+using Pokemon_Battle_Clone.Runtime.Core.Infrastructure.Types;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Pokemon_Battle_Clone.Runtime.Moves.Infrastructure
 {
     [RequireComponent(typeof(Button))]
-    public class MoveButton : MonoBehaviour
+    public class MoveButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        [SerializeField] private Image typeIcon;
+        [SerializeField] private Image background;
+        [SerializeField] private GameObject border;
         [SerializeField] private TextMeshProUGUI nameText;
         [SerializeField] private TextMeshProUGUI ppText;
+        
+        [Space(10)]
+        [SerializeField] private ElementalTypesConfig elementalTypesConfig;
 
         [HideInInspector] public int index;
         
@@ -31,6 +37,20 @@ namespace Pokemon_Battle_Clone.Runtime.Moves.Infrastructure
             ppText.text = $"{move.CurrentPP} / {move.MaxPP}";
 
             _button.interactable = move.CurrentPP > 0;
+            
+            typeIcon.sprite = elementalTypesConfig.GetIcon(move.Type);
+            background.color = elementalTypesConfig.GetColor(move.Type);
+        }
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            border.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            border.SetActive(false);
         }
     }
 }
