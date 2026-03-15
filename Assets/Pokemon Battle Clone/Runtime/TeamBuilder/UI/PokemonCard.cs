0f1616513c1600c071b9c006fef9e10239e35639
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-03-15 17:20:59 +0100

    feat: pokemon card

---
+++ Assets/Pokemon Battle Clone/Runtime/TeamBuilder/UI/PokemonCard.cs
@@ -0,0 +1,26 @@
+﻿using Pokemon_Battle_Clone.Runtime.Core.Domain;
+using Pokemon_Battle_Clone.Runtime.Moves.Infrastructure;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pokemon_Battle_Clone.Runtime.TeamBuilder.UI
+{
+    public class PokemonCard : MonoBehaviour
+    {
+        [SerializeField] private Image pokemonIcon;
+        [SerializeField] private TextMeshProUGUI pokemonName;
+        [SerializeField] private TextMeshProUGUI pokemonLevel;
+        [SerializeField] private PokemonCardStats stats;
+        [SerializeField] private MoveSetCard moveSet;
+        
+        public void SetInfo(Pokemon pokemon, Sprite icon)
+        {
+            pokemonIcon.sprite = icon;
+            pokemonName.text = pokemon.Name;
+            pokemonLevel.text = $"Lv. {pokemon.Stats.Level}";
+            stats.Display(pokemon.Stats.Stats);
+            moveSet.Display(MoveSetDTO.Get(pokemon.MoveSet));
+        }
+    }
+}
