c6cbe55f3e9826554742646f7a16b803c7d4be08
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-03-16 17:29:35 +0100

    feat: display team info from trainer selection

--- Assets/Pokemon Battle Clone/Runtime/TeamBuilder/Selector/TrainerTeamSelection.cs
+++ Assets/Pokemon Battle Clone/Runtime/TeamBuilder/Selector/TrainerTeamSelection.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Pokemon_Battle_Clone.Runtime.Database;
+using Pokemon_Battle_Clone.Runtime.TeamBuilder.TeamDisplayer;
 using Pokemon_Battle_Clone.Runtime.Trainers.Infrastructure.Actions;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace Pokemon_Battle_Clone.Runtime.TeamBuilder.Selector
 {
     public class TrainerTeamSelection : MonoBehaviour
     {
         [SerializeField] private PokemonAssetDatabase assetDatabase;
+        [SerializeField] private TeamInfoDisplayer teamInfoDisplayer;
         [SerializeField] private List<PokemonSelectorButton> pokemonButtons;
         
         private TeamConfig _teamConfig;
@@ -16,8 +20,12 @@ namespace Pokemon_Battle_Clone.Runtime.TeamBuilder.Selector
 
         private void Start()
         {
-            foreach (var button in pokemonButtons)
-                button.gameObject.SetActive(false);
+            for (var i = 0; i < pokemonButtons.Count; i++)
+            {
+                pokemonButtons[i].index = i;
+                pokemonButtons[i].gameObject.SetActive(false);
+                pokemonButtons[i].OnClick += OnInfoDisplayed;
+            }
         }
 
         public void SetTeam(TeamConfig teamConfig)
@@ -37,6 +45,12 @@ namespace Pokemon_Battle_Clone.Runtime.TeamBuilder.Selector
                     pokemonButtons[i].gameObject.SetActive(false);
                 }
             }
+        }
+
+        private void OnInfoDisplayed(int pokemonIndex)
+        {
+            var pokemonList = _teamConfig.Build().PokemonList.ToList();
+            teamInfoDisplayer.Display(pokemonList, pokemonIndex);
         }
     }
 }
